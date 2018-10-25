f5d3cd29c4821fe6e90391682446b29cc788bade
Umbersaprmaxym17 <maximsaprykin@yahoo.com>
2018-10-24 17:12:55 -0700

    even numbers up to given

---
+++ src/Numbers/EvenNumbersUpToGiven.java
@@ -0,0 +1,37 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class EvenNumbersUpToGiven {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number : ");
+        int number = sc.nextInt();
+        int i;
+        sc.close();
+        System.out.println("Even number are : ");
+
+
+       for (i = 0; i <= number; i++ )
+           if(i % 2 == 0 ){
+               System.out.println(i);
+           }
+
+
+
+
+
+
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
+}
