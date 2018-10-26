fe417093717c6acd052413607c34120c1d6335e5
Umbersaprmaxym17 <maximsaprykin@yahoo.com>
2018-10-25 21:40:41 -0700

    refactor 2

--- src/AlgoLeet/Solutions.java
+++ src/AlgoLeet/Solutions.java
@@ -9,23 +9,25 @@ public  class Solutions {
         public static List<String> fizzBuzz(int n) {
             List<String> result = new ArrayList<>();
 
-            for(int i = 1;i<=n; i++ ){
+            for (int i = 1; i <= n; i++) {
 
-                if (i % 3 ==0 && i % 5 ==0){
-                    result.add("Solutions");
-                }else if(i % 5 ==0){
-                    result.add("Buzz");
-                }else if( i % 3 ==0){
+                if (i % 3 == 0 && i % 5 == 0) {
+                    result.add("FizzBuzz");
+                } else if (i % 3 == 0) {
                     result.add("Fizz");
-                }else {
-                    result.add(String.valueOf(i));
-
+                } else if (i % 5 == 0) {
+                    result.add("Buzz");
+                } else {
+                    result.add(String.valueOf(i));  // 1 -> "1"
                 }
+            }
+            System.out.println(result);
+            return result;
 
-
-            }return result;
 
         }
+
+
 
 
 
