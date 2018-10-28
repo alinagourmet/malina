b3561f9bf90f83d57a009cb8eccb5da305e9bd0f
Umbersaprmaxym17 <maximsaprykin@yahoo.com>
2018-10-27 21:03:53 -0700

    second sum

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import AlgoLeet.Solutions;
 public class Main extends Solutions {
 
     public static void main(String[] args) {
+        int nums[] ={5, 4 ,7,2, 6,7};
+        twoSum(nums,9);
 
     }
 }
